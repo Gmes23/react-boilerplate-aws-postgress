fca4a2878e062690d42f71f9664761528ffd9b22
Gerardo  Mestanza <gmesda23@gmail.com>
2019-11-12 23:29:39 -0500

    started home shopping cart items , refactored home .js file

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Home = () => (
-  <h1><bold>WELCOME HOME</bold></h1>
-)
+class Home extends Component {
+  constructor(props) {
+    console.log(props)
+    super(props);
+    this.state={
+      homeItems: [{
+        id: '1',
+        name:'item 1', 
+        price: 'price',
+        picture: 'picture',
+      }]
+    }
+  }
+  render() {
+
+    console.log(this.state)
+    return(
+     
+      <div> 
+        <ul key={this.state.map(this)}/>
+        // WELCOME HOME 
+
+      </div>
+
+    )
+  }
+}
 
 export default Home
